9384dfb460ddc638e9a7204313e0b3518b0b1c13
Taylor Otwell <taylor@laravel.com>
2023-08-03 09:59:34 -0500

    Update VoltDirectiveMissingException.php

--- src/Exceptions/VoltDirectiveMissingException.php
+++ src/Exceptions/VoltDirectiveMissingException.php
@@ -12,7 +12,7 @@ class VoltDirectiveMissingException extends RuntimeException
     public function __construct(string $bladePath)
     {
         parent::__construct(
-            'The [@volt] directive is required when using Volt anonymous components in Folio pages, and is missing from ['.$bladePath.'].',
+            'The [@volt] directive is required when using Volt anonymous components in Folio pages. The directive is missing in ['.$bladePath.'].',
         );
     }
 }
