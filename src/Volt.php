898b46fe7db48afe5a6eca25b2dec82237b515ab
Robson Tenório <rrtenorio@gmail.com>
2023-09-23 16:31:31 -0300

    Add docblock for `Volt::withQueryParams()`
    Add docblock for `Volt::withQueryParams()`

--- src/Volt.php
+++ src/Volt.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Facade;
  * @method static void mount(array|string $paths = [], array|string $uses = [])
  * @method static array paths()
  * @method static \Livewire\Features\SupportTesting\Testable test(string $name, array $params = [])
+ * @method static self withQueryParams(array $params = [])
  * @method static \Illuminate\Routing\Route route(string $uri, string $componentName)
  */
 class Volt extends Facade
