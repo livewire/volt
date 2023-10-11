00d25a02a64d91f50e65ca5635066e615a820777
Nuno Maduro <enunomaduro@gmail.com>
2023-10-11 15:03:51 +0100

    Adjusts facade docblocks

--- src/Volt.php
+++ src/Volt.php
@@ -9,7 +9,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static void mount(array|string $paths = [], array|string $uses = [])
  * @method static array paths()
  * @method static \Livewire\Features\SupportTesting\Testable test(string $name, array $params = [])
- * @method static self withQueryParams(array $params = [])
+ * @method static static actingAs(\Illuminate\Contracts\Auth\Authenticatable $user, ?string $driver = null)
+ * @method static static withQueryParams(array $params = [])
  * @method static \Illuminate\Routing\Route route(string $uri, string $componentName)
  */
 class Volt extends Facade
