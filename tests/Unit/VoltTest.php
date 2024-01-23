c4333a1f9929ae17e62e827c4adc207b013a115f
= <inman.turbo@gmail.com>
2024-01-23 16:26:20 -0500

    add tests for Volt::mount()

--- tests/Unit/VoltTest.php
+++ tests/Unit/VoltTest.php
@@ -8,3 +8,39 @@ it('resolves an manager instance', function () {
 
     expect($instance)->toBeInstanceOf(VoltManager::class);
 });
+
+it('mounts paths into memory from strings', function () {
+    /** @var VoltManager $managerInstance */
+    $managerInstance = Volt::getFacadeRoot();
+
+    $managerInstance->mount($path1 = __DIR__ . '/resources/views/vendor/livewire');
+
+    expect(count($managerInstance->paths()))->toBe(1);
+
+    expect($path = (collect($managerInstance->paths())->first())->path)->toBe($path1);
+
+    $managerInstance->mount($path2 = __DIR__ . '/resources/views/livewire');
+
+    expect(count($managerInstance->paths()))->toBe(2);
+
+    expect($path = (collect($managerInstance->paths())->last())->path)->toBe($path2);
+});
+
+it('mounts paths into memory from arrays', function () {
+    /** @var VoltManager $managerInstance */
+    $managerInstance = Volt::getFacadeRoot();
+
+    $managerInstance->mount([
+        $path1 = __DIR__ . '/resources/views/vendor/livewire',
+    ]);
+
+    expect(count($managerInstance->paths()))->toBe(1);
+
+    expect($path = (collect($managerInstance->paths())->first())->path)->toBe($path1);
+
+    $managerInstance->mount([
+        $path2 = __DIR__ . '/resources/views/livewire',
+    ]);
+
+    expect($path = (collect($managerInstance->paths())->last())->path)->toBe($path2);
+});
