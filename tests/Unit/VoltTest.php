e982a76bdf33955149f62c63875f7d6e97df36fb
= <inman.turbo@gmail.com>
2024-01-23 17:50:39 -0500

    test for memory leaks

--- tests/Unit/VoltTest.php
+++ tests/Unit/VoltTest.php
@@ -54,9 +54,30 @@ it('mounts paths into memory from arrays', function () {
         $path2 = __DIR__ . '/resources/views/livewire',
     ]);
 
+    expect(count($managerInstance->paths()))->toBe(2);
+
     expect($path = (collect($managerInstance->paths())->last())->path)->toBe(str_replace(
         '/',
         DIRECTORY_SEPARATOR,
         $path2,
     ));
+});
+
+test('there are no memory leaks', function () {
+    /** @var VoltManager $managerInstance */
+    $managerInstance = Volt::getFacadeRoot();
+
+    expect(count($managerInstance->paths()))->toBe(0);
+
+    $managerInstance->mount([
+        $path1 = __DIR__ . '/resources/views/vendor/livewire',
+    ]);
+
+    $managerInstance->mount([
+        $path1
+    ]);
+
+    $managerInstance->mount($path1);
+
+    expect(count($managerInstance->paths()))->toBe(1);
 });
