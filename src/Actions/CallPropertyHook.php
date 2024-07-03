4518e79592d6a97af3d905b5cc2971d429115e8d
Ulrich Pascal Ndjike Zoa <ndjikezoaulrich@gmail.com>
2024-07-03 06:16:28 +0100

    Support deep array keys hook

--- src/Actions/CallPropertyHook.php
+++ src/Actions/CallPropertyHook.php
@@ -3,6 +3,7 @@
 namespace Livewire\Volt\Actions;
 
 use Closure;
+use Illuminate\Support\Arr;
 use Livewire\Volt\CompileContext;
 use Livewire\Volt\Component;
 use Livewire\Volt\Contracts\Action;
@@ -22,8 +23,17 @@ class CallPropertyHook implements Action
     public function execute(CompileContext $context, Component $component, array $arguments): mixed
     {
         if (! isset($context->{$this->hookName}[$this->propertyName]) && str_contains($this->propertyName, '.')) {
-            [$this->propertyName, $additionalArgument] = explode('.', $this->propertyName, 2);
-            $arguments = array_merge([$additionalArgument], $arguments);
+            $hookedProperties = array_keys($context->{$this->hookName});
+
+            $matchingProperty = Arr::first($hookedProperties, fn ($key) => str($this->propertyName)->is("$key*"));
+
+            if ($matchingProperty) {
+                $additionalArgument = str($this->propertyName)->after($matchingProperty)->chopStart('.')->value();
+
+                $this->propertyName = $matchingProperty;
+
+                $arguments = array_merge([$additionalArgument], $arguments);
+            }
         }
 
         $hook = $context->{$this->hookName}[$this->propertyName] ?? fn () => null;
--- tests/Feature/Actions/CallPropertyHookTest.php
+++ tests/Feature/Actions/CallPropertyHookTest.php
@@ -31,3 +31,31 @@ it('calls array property hooks on the component', function () {
 
     expect($result)->toBe('bar');
 });
+
+it('calls array property hooks for deep key changed on the component', function () {
+    $context = CompileContext::make();
+
+    $context->updating = ['foo' => fn ($prop) => $prop];
+
+    $component = new class extends Component
+    {
+    };
+
+    $result = (new CallPropertyHook('updating', 'foo.bar.baz'))->execute($context, $component, []);
+
+    expect($result)->toBe('bar.baz');
+});
+
+it('calls sub-key property hooks on the component', function () {
+    $context = CompileContext::make();
+
+    $context->updating = ['foo.bar' => fn ($prop) => $prop];
+
+    $component = new class extends Component
+    {
+    };
+
+    $result = (new CallPropertyHook('updating', 'foo.bar.baz'))->execute($context, $component, []);
+
+    expect($result)->toBe('baz');
+});
